4109f54d797ce1d43441ee57b45560bfc94c45ed
Volker Schmitz <v.schmitz@tarent.de>
2023-04-21 11:24:57 +0200

    switch to podman

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build shells/shellinabox1 -t local/shellinabox
+podman build shells/shellinabox1 -t local/shellinabox
 
-docker-compose up -d --remove-orphans
+podman-compose up -d --remove-orphans
